1135747458a924988e20b2aa3b46b43311fbe551
Neil <neilwilliams08@gmail.com>
2023-07-02 23:02:16 -0400

    redo4

--- client/main.js
+++ client/main.js
@@ -70,3 +70,70 @@ function fetchMotivationalImage() {
 
 complimentBtn.addEventListener('click', getCompliment);
 
+const nameInput = document.getElementById('nameInput');
+const affirmationButton = document.getElementById('affirmationButton');
+const affirmationResult = document.getElementById('affirmationResult');
+
+affirmationButton.addEventListener('click', getDailyAffirmation);
+
+function getDailyAffirmation() {
+  const name = nameInput.value;
+  const affirmations = [
+    `You, ${name}, are capable of achieving greatness every day!`,
+    `Believe in yourself, ${name}, for you have the power to overcome any challenge!`,
+    `Stay positive, ${name}, and watch how your life transforms for the better!`
+    // Add more affirmations as desired
+  ];
+  const randomIndex = Math.floor(Math.random() * affirmations.length);
+  const affirmation = affirmations[randomIndex];
+  affirmationResult.textContent = affirmation;
+}
+
+const imageUpload = document.getElementById('imageUpload');
+const imageDescription = document.getElementById('imageDescription');
+const addToVisionBoardButton = document.getElementById('addToVisionBoardButton');
+const visionBoard = document.getElementById('visionBoard');
+
+addToVisionBoardButton.addEventListener('click', addToVisionBoard);
+
+function addToVisionBoard() {
+  const file = imageUpload.files[0];
+  const description = imageDescription.value;
+  const imageURL = URL.createObjectURL(file);
+  const imageElement = document.createElement('img');
+  imageElement.src = imageURL;
+  imageElement.alt = description;
+  const captionElement = document.createElement('p');
+  captionElement.textContent = description;
+  visionBoard.appendChild(imageElement);
+  visionBoard.appendChild(captionElement);
+  imageDescription.value = '';
+}
+
+const createPost = () => {
+  const titleInput = document.getElementById('titleInput');
+  const bodyInput = document.getElementById('bodyInput');
+
+  const post = {
+    title: titleInput.value,
+    body: bodyInput.value
+  };
+
+  fetch('http://localhost:3000/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(post)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('New post created:', data);
+      // Do something with the response data
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+};
+
+
--- client/node.js
+++ client/node.js
@@ -1,24 +1,39 @@
-// server.js
-
 const express = require('express');
 const app = express();
 
-// Route handler for getting a random fortune
+const fortunes = [
+  "You will find success in unexpected places.",
+  "A thrilling adventure awaits you in the near future.",
+  "Good fortune will smile upon you today.",
+  "Your hard work will pay off and lead to great achievements.",
+  "Love and happiness will surround you in the coming days."
+];
+
 app.get('/fortune', (req, res) => {
-  const fortunes = [
-    "You will have a great day today.",
-    "A wonderful surprise is coming your way.",
-    "Good things come to those who wait.",
-    "Your hard work will pay off soon.",
-    "A new opportunity will arise in your life."
-  ];
   const randomIndex = Math.floor(Math.random() * fortunes.length);
   const fortune = fortunes[randomIndex];
   res.json({ fortune });
 });
 
-// Start the server
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+const express = require('express');
+const go = express();
+app.use(express.json());
+
+let posts = [];
+
+app.post('/posts', (req, res) => {
+  const { title, body } = req.body;
+  const newPost = { id: Date.now(), title, body };
+  posts.push(newPost);
+  res.json(newPost);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
 
